20d5e4cfff89b85cef6b6790a4cecd2d8edd3ff6
songawee <dennis@songawee.com>
2013-11-04 18:10:37 -0700

    Update package.json

--- index.js
+++ index.js
@@ -7,10 +7,20 @@ function convert(msg) {
     var font_index = raw_data.indexOf(search_string);
     var semi_colon = raw_data.indexOf(";", font_index);
     var substring = raw_data.substring(font_index, semi_colon);
-    var regexPattern = /\d{2}/; 
-    var numerical_string = substring.match(regexPattern);
-    var digit = numerical_string[0]/10;
-    console.log(digit + "rem;");
+    var regexPattern = /(font-size: \d+)/g; 
+    var numerical_string = substring.match(regexPattern) + "\n";
+  
+    fs.writeFile('style_new.css', numerical_string, function (err) {
+      if (err) throw err;
+      console.log('It\'s saved!'); 
+    });
+  
+    console.log(numerical_string);
+    /*
+    var pixel_end = substring.indexOf("p"); 
+    substring = substring.substring(numerical_string.index, pixel_end);
+    console.log(substring);
+    */
   }); 
 }
 
