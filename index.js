5a1341e0d98c995fce2342daabc9cb3e81456c60
songawee <dennis@songawee.com>
2013-11-10 13:40:01 -0700

    Made convert function async and finished replacement function

--- index.js
+++ index.js
@@ -3,24 +3,25 @@ var fs = require('fs');
 function convert(msg) {
   fs.readFile('./' + msg, 'utf8', function (err, data) {
     var raw_data = data;
-    var search_string = "font-size";
-    var font_index = raw_data.indexOf(search_string);
-    var semi_colon = raw_data.indexOf(";", font_index);
-    var substring = raw_data.substring(font_index, semi_colon);
-    var regexPattern = /(font-size: \d+)/g; 
-    var numerical_string = substring.match(regexPattern) + "\n";
-  
-    fs.writeFile('style_new.css', numerical_string, function (err) {
-      if (err) throw err;
-      console.log('It\'s saved!'); 
-    });
-  
-    console.log(numerical_string);
-    /*
-    var pixel_end = substring.indexOf("p"); 
-    substring = substring.substring(numerical_string.index, pixel_end);
-    console.log(substring);
-    */
+    var processed_data;
+    var regexPattern = /(font-size:) (\d+)(px)/gi; 
+
+    function parseString(match, css_style, amount, unit) {
+      amount = parseInt(amount, 10);
+      amount = amount/10;
+      amount = amount.toString();
+      unit = "rem";
+      match = "font-size:";
+ 
+      return match + " " + amount + unit;
+    } 
+		 
+    // pull out the px units and replace with rem units
+    processed_data = raw_data.replace(regexPattern, parseString);  
+
+ 		fs.writeFile('./style_new.css', processed_data, function() {
+			console.log("Party Onward with rem units :)");
+    }); 
   }); 
 }
 
