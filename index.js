979cbc4999b4d839b07dc3b44814498e2a861a7f
songawee <dennis@songawee.com>
2013-11-12 16:41:57 -0700

    Add findHTML function to adjust font-size styles within html style if found

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ function parseString(match, css_style, amount, unit) {
 
   return match + " " + amount + unit;
 } 
+
+function findHTML(input) {
+  if (input.search("html") == -1) {
+    // html style was not found, need to add it 
+    var html_style = "html {\n  font-size: 62.5%;\n}\n\n";
+    input = html_style + input; 
+    return input; 
+  } else {
+    // testing new regex for html-font sizes
+    var matches = input.match(/(html)(.*)({\n)(.*)(font-size:)(.*)(\d+)/g);  
+    console.log(matches);
+
+    return input;
+  }
+}
 				 
 function convert(msg) {
   fs.readFile('./' + msg, 'utf8', function (err, data) {
@@ -17,7 +32,8 @@ function convert(msg) {
         return err;
       }
 
-			var raw_data = data;
+      // add the html style to set root of font sizes
+			var raw_data = findHTML(data);
 			var processed_data;
 			var regexPattern = /(font-size:) (\d+)(px)/gi; 
 
