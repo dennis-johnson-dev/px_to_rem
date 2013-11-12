c59071775f581e9587d7c1e0b8131d438942aa3b
songawee <dennis@songawee.com>
2013-11-12 13:34:31 -0700

    Update err statements and moved parseString to a separate function

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 var fs = require('fs');
 
+function parseString(match, css_style, amount, unit) {
+  // Convert amount string to an integer, divide it by 10, and 
+  // convert amount back to a string
+  amount = ((parseInt(amount, 10))/10).toString();
+  unit = "rem";
+  match = "font-size:";
+
+  return match + " " + amount + unit;
+} 
+				 
 function convert(msg) {
   fs.readFile('./' + msg, 'utf8', function (err, data) {
+      if (err) { 
+        console.log("File couldn't be found or opened"); 
+        return err;
+      }
+
 			var raw_data = data;
 			var processed_data;
 			var regexPattern = /(font-size:) (\d+)(px)/gi; 
 
-			function parseString(match, css_style, amount, unit) {
-				amount = parseInt(amount, 10);
-				amount = amount/10;
-				amount = amount.toString();
-				unit = "rem";
-				match = "font-size:";
-	 
-				return match + " " + amount + unit;
-			} 
-			 
 			// pull out the px units and replace with rem units
 			processed_data = raw_data.replace(regexPattern, parseString);  
 
-			fs.writeFile('./style_new.css', processed_data, function() {
+			fs.writeFile('./style_new.css', processed_data, function(err) {
+				if (err) { 
+					console.log("File couldn't be saved");
+					return err; 
+				}
 				console.log("Party Onward with rem units :)");
 			}); 
+
   }); 
 }
 
